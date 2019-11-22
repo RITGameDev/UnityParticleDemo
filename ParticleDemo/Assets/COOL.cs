dea1bfc00453351276d79d9c4d18037149911c57
Dane <danemsherman@gmail.com>
2019-11-21 21:37:11 -0500

    Updated unity version and some effects

---
+++ ParticleDemo/Assets/COOL.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COOL : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    private ParticleSystem.Particle[] particles; //array of particles being controlled 
+
+    [SerializeField] private float particleSpeed = 1;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[ps.main.maxParticles];
+    }
+
+    private void Update()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = transform.position.z;
+
+        
+        int numParticles = ps.GetParticles(particles);
+        for(int i = 0; i< numParticles; i++)
+        {
+            ParticleSystem.Particle particle = particles[i];
+
+            //apply things to particles particle
+            Vector3 moveVector = mouseWorldPos - particle.position;
+
+            particle.velocity += ((moveVector.normalized * particleSpeed) - particle.velocity) * Time.deltaTime;
+
+            particles[i] = particle;
+        }
+        ps.SetParticles(particles);
+
+    }
+}
---
+++ ParticleDemo/Assets/Scripts/SeekMouse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class SeekMouse : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    private ParticleSystem.Particle[] particles; //array of particles being controlled 
+
+    [SerializeField] private float particleSpeed = 1;
+
+    [SerializeField] private Color32 close;
+    [SerializeField] private Color32 far;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[ps.main.maxParticles];
+    }
+
+    private void Update()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = transform.position.z;
+
+        int numParticles = ps.GetParticles(particles);
+        for (int i = 0; i < numParticles; i++)
+        {
+            ParticleSystem.Particle particle = particles[i];
+
+            Vector3 moveVector = mouseWorldPos - particle.position;
+
+            particle.velocity += ((moveVector.normalized * particleSpeed) - particle.velocity) * Time.deltaTime;
+
+            particle.color = moveVector.magnitude < 2 ? close : far; 
+
+            particles[i] = particle; //set the particle's data back into particles array
+        }
+
+        ps.SetParticles(particles, numParticles); //apply changes to particle system
+    }
+}
